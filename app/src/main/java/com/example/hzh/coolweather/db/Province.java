326071ed47f273d339f4114db859e3f66e34354d
BrightHe <15927305619@163.com>
2019-08-22 16:18:16 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/hzh/coolweather/db/Province.java
@@ -0,0 +1,45 @@
+package com.example.hzh.coolweather.db;
+
+/**
+ * Created by Administrator on 2019/8/22.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;//名称
+    private int provinceCode;//代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Province{" +
+                "id=" + id +
+                ", provinceName='" + provinceName + '\'' +
+                ", provinceCode=" + provinceCode +
+                '}';
+    }
+}
