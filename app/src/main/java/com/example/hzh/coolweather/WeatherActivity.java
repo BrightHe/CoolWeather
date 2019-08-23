eeb1f33bdd7051509c202eb6a7de918f2db44436
BrightHe <15927305619@163.com>
2019-08-23 11:22:48 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/hzh/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/hzh/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.hzh.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.hzh.coolweather.gson.Forecast;
 import com.example.hzh.coolweather.gson.HeWeather;
+import com.example.hzh.coolweather.service.AutoUpdateService;
 import com.example.hzh.coolweather.util.HttpUtil;
 import com.example.hzh.coolweather.util.Utility;
 
@@ -179,6 +181,9 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString( "weather",responseText );
                             editor.commit();
                             showWeatherInfo( weather );
+                            //开启定时更新天气的服务
+                            Intent intent = new Intent( WeatherActivity.this, AutoUpdateService.class );
+                            startService( intent );
                         }else {
                             Toast.makeText( WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT ).show();
                         }
